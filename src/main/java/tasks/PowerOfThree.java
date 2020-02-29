45efd84328bd12372c3a9750c60ca38134ff0d41
DmitryNuzhdin <kaf32dmitrii@gmail.com>
2020-02-29 22:11:02 +0300

    PowerOfThree and GreatestCommonDivider tasks

---
+++ src/main/java/tasks/PowerOfThree.java
@@ -0,0 +1,16 @@
+package tasks;
+
+public class PowerOfThree {
+    public static boolean powerOfThree(int a) {
+        /*
+        метод должен вернуть ответ - является -ли заданое число (a) степенью тройки
+        кстати в своем коде a можно юзать как любую другую переменную (ее можно менять)
+         */
+        return false;
+    }
+
+    public static void main(String[] arg) {
+        System.out.println(powerOfThree(0));
+    }
+}
+
