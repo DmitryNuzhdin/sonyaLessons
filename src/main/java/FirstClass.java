221c42977f2f0822cb7bcd6b431c6b7d775bd18f
sofidyak <sofidyak@gmail.com>
2020-02-29 15:12:26 +0300

    My first commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,28 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("First Class!");
+        int a = 1;
+        if (a==0){
+            a=5;
+        } else {
+            a=3;
+        }
+        for (int i=0; i<=20; i++) {
+            if (i % 3 == 0) {
+//                System.out.println(i);
+            }
+        }
+        for (int b=2; b<=100; b++) {
+            boolean p=true;
+            for (int c = 2; c < b; c++) {
+                if (b % c == 0) {
+                    p=false;
+                }
+            }
+            if (p) {
+                System.out.println(b);
+            }
+        }
+
+//        System.out.println(a+3);
     }
 }
